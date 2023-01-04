70340137f51f4bbdccdab0b2951f918cd9643667
kevinchege47 <chegekevin001@gmail.com>
2023-01-04 17:25:00 +0300

    Added H2 properties

---
+++ Spring-boot/src/main/java/com/kevin47/Springboot/controller/HelloController.java
@@ -0,0 +1,16 @@
+package com.kevin47.Springboot.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/")
+public String helloworld(){
+    return "Welcome to daily code buffer aaaa";
+}
+
+}
