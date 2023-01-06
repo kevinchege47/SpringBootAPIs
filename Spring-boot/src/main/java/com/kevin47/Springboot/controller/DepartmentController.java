66c39ccac4a1c1a3948653bdce5c73b9a9a480ba
kevinchege47 <chegekevin001@gmail.com>
2023-01-06 10:33:12 +0300

    Update Department by ID

--- Spring-boot/src/main/java/com/kevin47/Springboot/controller/DepartmentController.java
+++ Spring-boot/src/main/java/com/kevin47/Springboot/controller/DepartmentController.java
@@ -14,24 +14,28 @@ public class DepartmentController {
     private DepartmentService departmentService;
 
     @PostMapping("/departments")
-    public Department saveDepartment(@RequestBody Department department){
+    public Department saveDepartment(@RequestBody Department department) {
         return departmentService.saveDepartment(department);
     }
+
     @GetMapping("/departments")
-    public List<Department> fetchDepartmentList(){
+    public List<Department> fetchDepartmentList() {
         return departmentService.fetchDepartmentList();
     }
 
     @GetMapping("/departments/{id}")
-    public Department fetchDepartmentById(@PathVariable("id") Long departmentId){
+    public Department fetchDepartmentById(@PathVariable("id") Long departmentId) {
         return departmentService.fetchDepartmentById(departmentId);
     }
 
     @DeleteMapping("/departments/{id}")
-    public String deleteDepartmentById(@PathVariable("id") Long departmentId){
+    public String deleteDepartmentById(@PathVariable("id") Long departmentId) {
         departmentService.deleteDepartmentById(departmentId);
         return "Department Deleted Successfully";
+    }
 
-
+    @PutMapping("/departments/{id}")
+    public Department updateDepartment(@PathVariable("id") Long departmentId, @RequestBody Department department) {
+        return departmentService.updateDepartment(departmentId,department);
     }
 }
--- Spring-boot/src/main/java/com/kevin47/Springboot/service/DepartmentSeviceImpl.java
+++ Spring-boot/src/main/java/com/kevin47/Springboot/service/DepartmentSeviceImpl.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.DeleteMapping;
 
 import java.util.List;
+import java.util.Objects;
 
 @Service
 public class DepartmentSeviceImpl implements DepartmentService{
@@ -31,6 +32,25 @@ public class DepartmentSeviceImpl implements DepartmentService{
     @Override
     public void deleteDepartmentById(Long departmentId) {
          departmentRepository.deleteById(departmentId);
+    }
+
+    @Override
+    public Department updateDepartment(Long departmentId, Department department) {
+        Department depDB=departmentRepository.findById(departmentId).get();
+        if(Objects.nonNull(department.getDepartmentName()) && !"".
+                equalsIgnoreCase(department.getDepartmentName())){
+            depDB.setDepartmentName(department.getDepartmentName());
+        }
+        if(Objects.nonNull(department.getDepartmentCode()) && !"".
+                equalsIgnoreCase(department.getDepartmentCode())){
+            depDB.setDepartmentCode(department.getDepartmentCode());
+        }
+        if(Objects.nonNull(department.getDepartmentAddress()) && !"".
+                equalsIgnoreCase(department.getDepartmentAddress())){
+            depDB.setDepartmentAddress(department.getDepartmentAddress());
+        }
+
+        return departmentRepository.save(depDB);
     }
 
 
