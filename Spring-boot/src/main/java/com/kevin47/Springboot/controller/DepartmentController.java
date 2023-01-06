e7c019e0dcd519f81679f271dfbde67de79c80ec
kevinchege47 <chegekevin001@gmail.com>
2023-01-06 11:56:13 +0300

    Added Logs

--- Spring-boot/src/main/java/com/kevin47/Springboot/controller/DepartmentController.java
+++ Spring-boot/src/main/java/com/kevin47/Springboot/controller/DepartmentController.java
@@ -3,6 +3,9 @@ package com.kevin47.Springboot.controller;
 import com.kevin47.Springboot.entity.Department;
 import com.kevin47.Springboot.service.DepartmentService;
 import com.kevin47.Springboot.service.DepartmentSeviceImpl;
+import jakarta.validation.Valid;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,34 +15,41 @@ import java.util.List;
 public class DepartmentController {
     @Autowired
     private DepartmentService departmentService;
+    private final Logger LOGGER = LoggerFactory.getLogger(DepartmentController.class);
 
     @PostMapping("/departments")
-    public Department saveDepartment(@RequestBody Department department) {
+    public Department saveDepartment(@Valid @RequestBody Department department) {
+        LOGGER.info("Inside save department of department controller");
         return departmentService.saveDepartment(department);
     }
 
     @GetMapping("/departments")
     public List<Department> fetchDepartmentList() {
+        LOGGER.info("Inside fetchDepartmentList of department controller");
         return departmentService.fetchDepartmentList();
     }
 
     @GetMapping("/departments/{id}")
     public Department fetchDepartmentById(@PathVariable("id") Long departmentId) {
+        LOGGER.info("Inside fetchDepartmentById of department controller");
         return departmentService.fetchDepartmentById(departmentId);
     }
 
     @DeleteMapping("/departments/{id}")
     public String deleteDepartmentById(@PathVariable("id") Long departmentId) {
+        LOGGER.info("Inside deleteDepartmentById of department controller");
         departmentService.deleteDepartmentById(departmentId);
         return "Department Deleted Successfully";
     }
 
     @PutMapping("/departments/{id}")
     public Department updateDepartment(@PathVariable("id") Long departmentId, @RequestBody Department department) {
+        LOGGER.info("Inside updateDepartment of department controller");
         return departmentService.updateDepartment(departmentId,department);
     }
     @GetMapping("/departments/name/{name}")
     public Department fetchDepartmentByName(@PathVariable("name") String departmentName){
+        LOGGER.info("Inside fetchDepartmentByName of department controller");
         return departmentService.fetchDepartmentByName(departmentName);
 
 
--- Spring-boot/src/main/java/com/kevin47/Springboot/repository/DepartmentRepository.java
+++ Spring-boot/src/main/java/com/kevin47/Springboot/repository/DepartmentRepository.java
@@ -3,10 +3,15 @@ package com.kevin47.Springboot.repository;
 import com.kevin47.Springboot.entity.Department;
 import org.aspectj.lang.reflect.DeclareAnnotation;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface DepartmentRepository extends JpaRepository<Department,Long> {
 public Department findByDepartmentName(String departmentName);
+//JPQL Query
+//@Query("?1")
+//SQL QUERY
+//@Query(value = "",nativeQuery = true)
 public Department findByDepartmentNameIgnoreCase(String departmentName);
 }
