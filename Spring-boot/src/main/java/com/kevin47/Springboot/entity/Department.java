7f8c74e1be772387d75554fa83cb02d28ca02892
kevinchege47 <chegekevin001@gmail.com>
2023-01-10 19:21:43 +0300

    department service test

--- Spring-boot/src/main/java/com/kevin47/Springboot/entity/Department.java
+++ Spring-boot/src/main/java/com/kevin47/Springboot/entity/Department.java
@@ -6,6 +6,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -13,6 +14,7 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class Department {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
---
+++ Spring-boot/src/test/java/com/kevin47/Springboot/service/DepartmentServiceTest.java
@@ -0,0 +1,40 @@
+package com.kevin47.Springboot.service;
+
+import com.kevin47.Springboot.entity.Department;
+import com.kevin47.Springboot.repository.DepartmentRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import static org.junit.jupiter.api.Assertions.*;
+@SpringBootTest
+class DepartmentServiceTest {
+    @Autowired
+    private DepartmentService departmentService;
+    @MockBean
+    private DepartmentRepository departmentRepository;
+
+    @BeforeEach
+    void setUp() {
+        Department department = Department.builder()
+                .departmentName("IT")
+                .departmentAddress("Nairobi")
+                .departmentCode("IT-06")
+                .departmentId(1l)
+                .build();
+        Mockito.when(departmentRepository.findByDepartmentNameIgnoreCase("IT"))
+                .thenReturn(department);
+
+    }
+    @Test
+    public void whenValidDepartmentName_thenDepartmentShouldFound(){
+        String departmentName = "IT";
+        Department found = departmentService.fetchDepartmentByName(departmentName);
+
+        assertEquals(departmentName,found.getDepartmentName());
+
+    }
+}
