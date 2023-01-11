ffb54301e2624f276a51922965c5567e5ce75e18
kevinchege47 <chegekevin001@gmail.com>
2023-01-11 21:58:38 +0300

    Added application yml file

--- Spring-boot/src/main/java/com/kevin47/Springboot/controller/DepartmentController.java
+++ Spring-boot/src/main/java/com/kevin47/Springboot/controller/DepartmentController.java
@@ -14,6 +14,7 @@ import java.util.List;
 
 @RestController
 public class DepartmentController {
+
     @Autowired
     private DepartmentService departmentService;
     private final Logger LOGGER = LoggerFactory.getLogger(DepartmentController.class);
--- Spring-boot/src/main/java/com/kevin47/Springboot/controller/HelloController.java
+++ Spring-boot/src/main/java/com/kevin47/Springboot/controller/HelloController.java
@@ -1,5 +1,6 @@
 package com.kevin47.Springboot.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,6 +9,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+    @Value("${welcome.message}")
+    private String welcomeMessage;
     @GetMapping("/")
 public String helloworld(){
     return "Welcome to daily code buffer aaaa";
